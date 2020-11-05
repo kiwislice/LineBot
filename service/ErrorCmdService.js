0c7cb1be007f0aaf8bd2e6827dc26f9d8b20b920
patrick <patrick.hu@fis.com.tw>
2020-11-05 11:16:31 +0800

    heroku test

--- service/ErrorCmdService.js
+++ service/ErrorCmdService.js
@@ -7,13 +7,7 @@ https://github.com/kiwislice/LineBot/blob/master/echo-bot/README.md
 
 service.handle = function (cmd, event) {
     if (cmd.startsWith('bot')) {
-        event.reply(msg).then(function (data) {
-            // success
-            // console.log(`event.reply success ${data}`);
-        }).catch(function (error) {
-            // error
-            // console.log(`event.reply error ${error}`);
-        });
+        event.reply(msg);
     }
 };
 
