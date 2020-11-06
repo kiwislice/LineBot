e1e0c9de7cf822f7b0e8a07825319f7bd2e34370
patrick <patrick.hu@fis.com.tw>
2020-11-06 17:12:42 +0800

    補上缺的檔案

---
+++ service/LeaveRoomService.js
@@ -0,0 +1,22 @@
+var service = { bot: null };
+
+const SERVICE_ID = `LeaveRoomService`;
+
+const tools = require("./Tools");
+
+service.handle = async function (cmd, event, bot) {
+  var sourceId = tools.getSourceId(event);
+  if (sourceId == null) return false;
+
+  if (cmd === "助理，你被開除了") {
+    event.reply("好，我走！！！");
+    setTimeout(async () => {
+      await tools.leaveRoom(event);
+    }, 1000);
+    return true;
+  }
+  return false;
+};
+
+
+module.exports = service;
