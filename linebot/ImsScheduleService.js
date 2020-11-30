af737e2bf7be1ec1a8460ccec01e53fcdcc13a56
patrick <patrick.hu@fis.com.tw>
2020-11-30 10:34:34 +0800

    修正IMS回報問題

--- linebot/ImsScheduleService.js
+++ linebot/ImsScheduleService.js
@@ -6,23 +6,42 @@ const tools = require("../service/Tools");
 const schedule = require("node-schedule");
 const axios = require("axios");
 var cache = {};
-const JOB_SETTING = "00 10 24-31 * 1-5";
+const JOB_SETTING = "35 10 24-31 * 1-5";
+// const JOB_SETTING = "0 * * * * 1-5";
+
+
 // 啟動時自動觸發排程
 repository.getSubscribedUserId({ service_id: SERVICE_ID }, (response) => {
   var list = response.data.data.linebot_subscribed;
   list.forEach((elm) => {
-    cache[elm.user_id] = function () {
-      service.bot.push(elm.user_id, `已啟動IMS回報`);
-      schedule.scheduleJob(JOB_SETTING, function () {
+    cache[elm.user_id] = schedule.scheduleJob(JOB_SETTING, function () {
+      console.log(`schedule run ${SERVICE_ID}`);
+      var date = new Date();
+      var today = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      var lastWeekDay = today.getDate() - today.getDay() + 1;
+      if (lastWeekDay <= date.getDate()) {
         service.bot.push(
           elm.user_id,
-          `今天是本月最後一個工作天,請各位記得上IMS回報每月工時.`
+          `本週是這個月的最後一個工作週，請記得在本月的最後一天上IMS回報工作時數，若有請假的請提前回報!!!.`
         );
-        cache[sourceId]();
-      });
-    };
-    console.log(`ImsScheduleService add cache ${elm.user_id}`);
+      }
+    })
   });
+
+
+  // cache[elm.user_id] = function () {
+  //   console.log(`cache[elm.user_id] = function ()`);
+  //   service.bot.push(elm.user_id, `已啟動IMS回報`);
+  //   schedule.scheduleJob(JOB_SETTING, function () {
+  //     console.log(`schedule run ${SERVICE_ID}`);
+  //     service.bot.push(
+  //       elm.user_id,
+  //       `今天是本月最後一個工作天,請各位記得上IMS回報每月工時.`
+  //     );
+  //     cache[sourceId]();
+  //   });
+  // };
+  console.log(`ImsScheduleService add cache ${elm.user_id}`);
 });
 
 service.handle = async function (cmd, event) {
@@ -36,6 +55,7 @@ service.handle = async function (cmd, event) {
     cache[sourceId] = function () {
       event.reply(`已啟動IMS回報`);
       schedule.scheduleJob(JOB_SETTING, function () {
+        console.log(`schedule run ${SERVICE_ID}`);
         var date = new Date();
         var today = new Date(date.getFullYear(), date.getMonth() + 1, 0);
         var lastWeekDay = today.getDate() - today.getDay() + 1;
@@ -44,9 +64,7 @@ service.handle = async function (cmd, event) {
             sourceId,
             `本週是這個月的最後一個工作週，請記得在本月的最後一天上IMS回報工作時數，若有請假的請提前回報!!!.`
           );
-          cache[sourceId]();
         }
-
       });
     };
     cache[sourceId]();
